d41dfaf1d1797750f28cbb3b906488523b4ddbf2
Lming1 <spear131121@gmail.com>
2017-12-15 19:20:55 +0900

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,32 @@
 console.log('my-note app.js');
+// var FileSaver = require('file-saver');
 
 // memo
 $(function () {
     if(localStorage.getItem('note')) {
         $('#memo').val(localStorage.getItem('note'));
     }
+    //about note
+    $('.btn-about').paulund_modal_box();
 });
 
 // btn-newnote 적용
-$(".btn-newnote").on("click", function(){
+$('.btn-newnote').on("click", function(){
     window.localStorage.clear();
     location.reload();
     console.log("new note");
     return false;
 });
 
-$(".btn-download").on("click",function(){
+// download note
+$('.btn-download').on("click",function(){
   console.log("note download");
+  try {
+        var b = new Blob([localStorage.getItem('note')],{type:"text/plain;charset=utf-8"});
+        saveAs(b,"test.txt");
+      }catch(e){
+        window.open("data:"+"text/plain;charset=utf-8"+"," + encodeURIComponent(localStorage.getItem('note')), '_blank','');
+      }
 });
 
 // savenote
@@ -28,7 +38,7 @@ $('.btn-savenote').click( function() {
 });
 
 // fullscreen
-$(".btn-fullscreen").click(function () {
+$('.btn-fullscreen').click(function () {
     console.log("fullscreen");
     if (screenfull.enabled) {
 		screenfull.request();
