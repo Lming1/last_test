59863579121ddd558f21228989bee1c7df727d8d
Lming1 <spear131121@gmail.com>
2017-12-15 15:11:22 +0900

    btn -> js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
 console.log('my-note app.js');
+
+
+// btn-newnote 적용
+
+$(".btn-newnote").on("click", function(){
+    console.log("new note");
+    return false;
+});
+
+// savenote
+$('.btn-savenote').click( function() {
+    console.log("save note");
+    return false;
+});
+
+// fullscreen
+$(".btn-fullscreen").click(function () {
+    console.log("fullscreen");
+});
