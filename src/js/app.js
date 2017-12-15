26746f0588acf563f76ac9c2008eb4a5d35d125d
Lming1 <spear131121@gmail.com>
2017-12-15 15:20:59 +0900

    btn 연결 + download

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,28 @@
 console.log('my-note app.js');
 
+// memo
+$(function () {
+    if(localStorage.getItem('note')) {
+        $('#memo').val(localStorage.getItem('note'));
+    }
+});
 
 // btn-newnote 적용
-
 $(".btn-newnote").on("click", function(){
+    window.localStorage.clear();
+    location.reload();
     console.log("new note");
     return false;
 });
 
+$(".btn-download").on("click",function(){
+  console.log("note download");
+});
+
 // savenote
 $('.btn-savenote').click( function() {
+    var note = $('#memo').val();
+    localStorage.setItem('note', note);
     console.log("save note");
     return false;
 });
