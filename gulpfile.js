382c1f06e2a453cad41c09aa7620e90101c3382c
Lming1 <spear131121@gmail.com>
2017-12-17 22:30:56 +0900

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,60 @@
 //gulpfile.js
 
 var gulp = require('gulp');
-var uglify  = require('gulp-uglify');
-var pump = require('pump');
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
 var watch = require('gulp-watch');
+var cleanCSS = require('gulp-clean-css');
+var connect = require('gulp-connect');
 
-
-var jsSrc = [
+var jsfiles = [
   'src/js/FileSaver.js',
   'src/js/screenfull.js',
   'src/js/app.js',
   'src/js/paulund_modal_box.js',
 ];
-gulp.task('js', function(cb) {
-  // place code for your default task here
-  // console.log('default')//
-  return gulp.src(jsSrc)
-            .pipe(concat('app.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest('dist/js'));
+gulp.task('concat:js', function() {
+  return gulp.src(jsfiles)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist/js'));
 });
+
 gulp.task('copy:html', function() {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist/'));
+  return gulp.src('src/index.html')
+    .pipe(connect.reload())
+    .pipe(gulp.dest('dist/'));
 });
-gulp.task('minify:css', function() {
-    return gulp.src('src/css/*.css')
-        .pipe(gulp.dest('dist/css'));
-});
+
 gulp.task('copy:img', function() {
     return gulp.src('src/js/images/*')
         .pipe(gulp.dest('dist/js/images'));
 });
 
+gulp.task('minify:js', function() {
+  return gulp.src('dist/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
 
-gulp.task('copy', ['copy:html','copy:img']);
-gulp.task('build', ['js', 'copy', 'minify:css']);
+gulp.task('minify:css', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 3001
+  });
+});
+
+
+gulp.task('copy', ['copy:html', 'copy:img']);
+gulp.task('build', ['concat:js', 'copy', 'minify:css']);
+gulp.task('server', ['connect'])
+gulp.task('default', ['build', 'watch', 'server']);
